4e514b0cb69d011334ec7435f52b9b128df60bfb
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-09-22 21:48:50 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VisualStudio_Github_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Update One");
+        }
+    }
+}
